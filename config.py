bbf80dd535b14e2987f415a1d76b6f41229052d1
Radomir Sudoreboot <55040629+1Rayko@users.noreply.github.com>
2024-03-14 09:42:29 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+mode = 1 # 1 - отправка в личные сообщения; 2 - отправка публикации
+
+# Vk
+user_id =  # user_id пользователя, которому будут приходить сообщения(Если нет, то  None)
+group_id= #  id сообщества, в котором будет создаваться публикация (Если нет, то  None)
+
+vk_token="" # токен пользователя или сообщества
+
+# Imgur
+# READ  https://apidocs.imgur.com/
+# https://api.imgur.com/oauth2/addclient
+imgur_client_id=''
+imgur_client_secret=''
+
+#Telegram
+bot_token='' # токен телеграм бота
